5fbf27393617861287f6a1a99304f24be14e0817
Ell <fachry2710@gmail.com>
2025-04-24 15:54:42 +0700

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
-  title: 'Nim - Personal website template',
+  title: 'FairyCel - a private story for celia',
   description:
     'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
 }
